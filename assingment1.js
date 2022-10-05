a585c269e6543ff99ba1ed8d25ce13a9a25fd651
pravinp3536 <shitalpatilpawar97@gmail.com>
2022-10-05 07:25:27 +0530

    basic command tried
    basic command tried

---
+++ assingment1.js
@@ -0,0 +1,15 @@
+console.log("Shital Suresh Patil");
+console.log("My hobbies are:-");
+console.log("1.Cooking");
+console.log("2.Gardening");
+console.log("3.I like to celebrate all the festivals");
+console.info("Personal Strengths:-");
+console.info("Good grasping power");
+console.info("Easily adjustable with new environment");
+console.info("Actively taking initiative in different activities");
+console.warn("Personal Weaknesses:");
+console.warn("Easily i am putting trust on people");
+console.warn("I am not good in english for overcoming this weakness i started to talk on english with family members and friends.");
+console.warn("I am weak in javascript so i joined the online classes to explore the javascript concepts");
+console.log(30+50);
+console.log(49*479);
