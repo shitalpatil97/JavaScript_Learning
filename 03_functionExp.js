292e5024ea91e2be34af221f277a50a292ac8526
pravinp3536 <shitalpatilpawar97@gmail.com>
2022-10-10 09:01:26 +0530

    function exp and string concepts we learned
    function exp and string concepts we learned

---
+++ 03_functionExp.js
@@ -0,0 +1,6 @@
+var test = function(arg1, arg2){
+    console.log("Function expression", arg1);
+    return "FE";
+}
+var returnValue = test(10, 20);
+console.log(returnValue);
---
+++ 03_string.js
@@ -0,0 +1,6 @@
+var str="Good Morning";
+var str1="Shital";
+console.log(str.length);
+console.log(str.charAt(1));
+console.log(str.concat(str1));
+console.log(str.indexOf('Mor'))
